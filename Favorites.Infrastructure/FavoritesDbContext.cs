b35547f3b6686a7e38ae1b5221cc653740f01068
domleggio <domleggio@gmail.com>
2024-09-30 22:46:58 -0700

    wip

--- Favorites.Infrastructure/FavoritesDbContext.cs
+++ Favorites.Infrastructure/FavoritesDbContext.cs
@@ -7,7 +7,7 @@ public class FavoritesDbContext : DbContext
 { 
     public FavoritesDbContext (DbContextOptions<FavoritesDbContext> options) : base(options) { }
 
-    public virtual DbSet<User> User { get; set; }
+    public virtual DbSet<User> User1 { get; set; }
     public virtual DbSet<Favorite> Favorite { get; set; }
     public virtual DbSet<Category> Category { get; set; }
 
